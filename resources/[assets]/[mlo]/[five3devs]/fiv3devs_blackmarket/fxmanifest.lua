32b44943093b8bc11cb8ac66f25e03081b47375c
MalmoCityRP <malmocityfounder@gmail.com>
2023-12-07 08:13:06 -0800

    Black Market + Bob74

---
+++ resources/[assets]/[mlo]/[five3devs]/fiv3devs_blackmarket/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+files {		
+	"audio/*.dat151.rel"
+}
+
+data_file "AUDIO_GAMEDATA" "audio/blackmarket_game.dat"
+data_file "AUDIO_GAMEDATA" "audio/blackmarketdoors_game.dat"
+
+dependency '/assetpacks'
