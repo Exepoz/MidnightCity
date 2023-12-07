248771589b3278f59967649d8a6dea8d439df296
MalmoCityRP <malmocityfounder@gmail.com>
2023-12-07 08:07:08 -0800

    Assets

---
+++ resources/[assets]/[peds]/pw_jiwon/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Pewe Koo <https://fivem.peweko.com/>'
+description 'Peds FiveM By Pewekoo'
+version '1.0.0'
+
+-- What to run
+files {
+	'peds.meta'
+}
+
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
+dependency '/assetpacks'
---
+++ resources/[assets]/[props]/mdn-props/fxmanifest.lua
@@ -0,0 +1,68 @@
+fx_version "cerulean"
+
+game "gta5"
+lua54 'yes'
+
+-- files {
+--     "stream/[bankprops]/ch_prop_gold_trolly_empty.ydr",
+--     "stream/[bankprops]/ch_prop_gold_trolly_empty.ytd"
+-- }
+
+--- Gas Stations Props
+data_file 'DLC_ITYP_REQUEST' 'stream/[electric_nozzle]/electric_nozzle_typ.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/[electric_charger]/electric_charger_typ.ytyp'
+
+-- data_file "DLC_ITYP_REQUEST" "stream/[bankprops]/props.ytyp"
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[bzzz]/bzzz_drink_props_juicepack.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[bzzz]/bzzz_animal_fish002.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[bzzz]/bzzz_props_lockpick_pack.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[bzzz]/bzzz_restaurant_foodpack.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[bzzz]/bzzz_prop_restaurant_menu.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[bzzz]/bzzz_plant_coca_a.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[bzzz]/bzzz_plants_weed_props.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[bzzz]/bzzz_prop_plant_weed_pot.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[bzzz]/bzzz_props_gardenpack.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_props_beehive_pack.ytyp'
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/breadroll.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/brioche2ytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/cakeytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/cerealytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/cheesewedgeytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/cheesewheelytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/decorativegourdytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/loafbreadytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/morange.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/potatoytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/redpepperytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/russetpotatoytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/subsandwichytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/pineapplebytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/pineappleaytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/jackfruitytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/jackfruitaytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/papayabytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/papayaaytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/sinkistbytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/sunskistaytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/dragunfruitbytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/dragonfruita.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/nectarinesaytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/donutytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/breadytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/crossiantytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/flanytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/pizzaytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/pieytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/barytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/bar-2ytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/bpastryytp.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[CL]/oatcookieytp.ytyp'
+
+
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[Valuables]/cash_bundles.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[Valuables]/cash_piles.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[Valuables]/cash_rolls.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[Valuables]/misc_valuables.ytyp'
