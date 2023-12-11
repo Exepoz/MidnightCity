a5ba2fa1f1ea344be99c0c7e3a844341e753daf9
MalmoCityRP <malmocityfounder@gmail.com>
2023-12-11 13:00:42 -0800

    Removed Stuff

--- resources/[uft]/[jobs]/dream-postal/target/target.lua
+++
@@ -1,9 +0,0 @@
-TARGET = {}
-
-function TARGET.IsOX()
-    return GetResourceState("ox_target") ~= "missing" or Config.TARGET == 'ox'
-end
-
-function TARGET.IsQB()
-    return GetResourceState("qb-target") ~= "missing" or Config.TARGET == 'qb-target'
-end
