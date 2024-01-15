cc8a27d222a7860ae87c740a709b1c33d0817cad
MalmoCityRP <malmocityfounder@gmail.com>
2024-01-15 11:07:06 -0800

    clothes 1

---
+++ resources/[assets]/[clothing]/ems2/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Generated with AltTool
+
+fx_version 'bodacious'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_pillbox.meta',
+  'mp_f_freemode_01_mp_f_pillbox.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_pillbox.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_pillbox.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
