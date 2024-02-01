d407d6389513e481e1d87fe8ebe39108631cd425
MalmoCityRP <malmocityfounder@gmail.com>
2024-02-01 10:00:51 -0800

    Body Disposal

--- resources/[midnight]/mdn-extras/server/sv_disposal.lua
+++ resources/[midnight]/mdn-extras/server/sv_disposal.lua
@@ -1,13 +1,15 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local Disposal = {}
+local needPay = {}
 for k, _ in pairs(Config.Disposals) do Disposal[k] = 0 end
 GlobalState.Disposals = Disposal
+GlobalState.DispoNeedPay = needPay
 
--- Use the phone
-QBCore.Functions.CreateUseableItem('hightech_phone', function(source, item)
-    if not QBCore.Functions.GetPlayer(source).Functions.GetItemByName('hightech_phone') then return end
-    TriggerClientEvent('mdn-extras:client:useHTPhone', source)
-end)
+-- -- Use the phone
+-- QBCore.Functions.CreateUseableItem('hightech_phone', function(source, item)
+--     if not QBCore.Functions.GetPlayer(source).Functions.GetItemByName('hightech_phone') then return end
+--     TriggerClientEvent('mdn-extras:client:useHTPhone', source)
+-- end)
 
 -- Disposal Logic & Send to Hospital
 RegisterNetEvent('mdn-extras:server:disposeBody', function(setting, ply)
@@ -16,21 +18,40 @@ RegisterNetEvent('mdn-extras:server:disposeBody', function(setting, ply)
     GlobalState.Disposals = Disposal
     if Disposal[setting] <= 0 then TriggerClientEvent('malmoo-extras:client:endDisposal', -1) end
     TriggerClientEvent('hospital:client:RespawnAtHospital', ply)
+    TriggerClientEvent('hospital:client:RespawnAtMorgue', ply)
+end)
+
+-- Pay for the disposal
+QBCore.Functions.CreateCallback('mdn-extras:server:setupDispo', function(source, cb, setting, amt)
+    if Disposal[setting] > 0 then TriggerClientEvent('QBCore:Notify', source, "This meal is not available at the moment.", 'success') cb(false) return end
+    amt = tonumber(amt)
+    local cost = amt * 1000
+    needPay[setting] = amt
+    GlobalState.DispoNeedPay = needPay
+    TriggerClientEvent('QBCore:Notify', source, 'Diner Reservation will cost '..cost.." crumbs. Meet at the location.", 'success')
+    cb(cost)
 end)
 
 -- Pay for the disposal
 QBCore.Functions.CreateCallback('mdn-extras:server:payDispo', function(source, cb, setting, amt)
     local Player = QBCore.Functions.GetPlayer(source)
-    amt = tonumber(amt)
-    local cost = amt * 5
-    if not Player.Functions.RemoveItem('hightech_phone', 1) then return end
-    if Player.Functions.RemoveMoney('cosmo', cost) then
-        Disposal[setting] = amt
-        GlobalState.Disposals = Disposal
-        TriggerClientEvent('QBCore:Notify', source, 'Diner Reservation set for '..amt..". Meet at the location.", 'success')
-        cb(true)
+    if Disposal[setting] > 0 then cb(true)
+    elseif needPay[setting] then
+        amt = tonumber(needPay[setting])
+        local cost = amt * 1000
+        if Player.Functions.RemoveItem('midnight_crumbs', cost) then
+            Disposal[setting] = amt
+            needPay[setting] = nil
+            GlobalState.Disposals = Disposal
+            GlobalState.DispoNeedPay = needPay
+            TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['midnight_crumbs'], "remove", cost)
+            TriggerClientEvent('QBCore:Notify', source, "Diner Reservation Paid. You can proceed with your guests.", 'success')
+            cb(true)
+        else
+            TriggerClientEvent('QBCore:Notify', source, "You don't have enough crumbs to complete this Transaction", 'error')
+            cb(false)
+        end
     else
-        TriggerClientEvent('QBCore:Notify', src, "You don't have enough Cosmo to complete this Transaction", 'error')
         cb(false)
     end
 end)
