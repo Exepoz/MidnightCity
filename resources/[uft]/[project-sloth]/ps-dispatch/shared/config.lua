ad756c235f3036d9e6965ad71e314685dc75b908
MalmoCityRP <malmocityfounder@gmail.com>
2024-02-09 11:50:24 -0800

    Update config.lua

--- resources/[uft]/[project-sloth]/ps-dispatch/shared/config.lua
+++ resources/[uft]/[project-sloth]/ps-dispatch/shared/config.lua
@@ -35,7 +35,7 @@ Config.EnableHuntingBlip = true
 
 Config.Locations = {
     ["HuntingZones"] = {
-        [1] = {label = "Hunting Zone", radius = 650.0, coords = vector3(-938.61, 4823.99, 313.92)},
+        --[1] = {label = "Hunting Zone", radius = 650.0, coords = vector3(-938.61, 4823.99, 313.92)},
     },
     ["NoDispatchZones"] = {
         [1] = {label = "Ammunation 1", coords = vector3(13.53, -1097.92, 29.8), length = 14.0, width = 5.0, heading = 70, minZ = 28.8, maxZ = 32.8},
